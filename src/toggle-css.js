a217e3fdf2776ed2a49449230c63492f23cd5f23
oifj34f34f <128177553+oifj34f34f@users.noreply.github.com>
2024-03-04 13:28:02 +0300

    Add support for inline styles

--- src/toggle-css.js
+++ src/toggle-css.js
@@ -3,6 +3,18 @@ function toggleCss(request) {
     for (const s of document.styleSheets) {
         s.disabled = request.disableCss;
     }
+
+    if (request.disableCss) {
+        document.querySelectorAll('[style]').forEach(el => {
+            el.setAttribute('data-original-style', el.getAttribute('style'));
+            el.removeAttribute('style');
+        });
+    } else {
+        document.querySelectorAll('[data-original-style]').forEach(el => {
+            el.setAttribute('style', el.getAttribute('data-original-style'));
+            el.removeAttribute('data-original-style');
+        });
+    }
 }
 
 browser.runtime.onMessage.addListener(toggleCss);
